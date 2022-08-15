3969e389fb8d28d6c4d51634a6ab4b456a13f3be
Julián Santos <ingjuliansantos@gmail.com>
2022-08-15 17:31:09 -0500

    API first run with the data schema

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
+from typing import List
+from pydantic import BaseModel
+from fastapi import status
 
 app = FastAPI()
 
+class Data(BaseModel):
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    Date: str 
+    daily_generacion: float 
+    daily_consumo_combustible: float
+    daily_demanda: float
+    daily_aportes_energia: float
+    daily_volumen_util_energia: float
+    daily_disponibilidad_real: float
+    daily_precio_bolsa: float
+    daily_emision_CO2_eq: float
 
+
+
+@app.get("/model/k", response_model=List[Data], status_code=status.HTTP_200_OK)
+def get_prediction(k: int, test_data: List[Data]):
+
+
+
+    return test_data
